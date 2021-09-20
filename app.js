847bdef2f9c2df3b4f4f7b4105903d0c3f3f6aad
Kenan Casey <kenancasey@gmail.com>
2021-09-20 08:28:11 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const toggleButton = document.querySelector(".toggle");
+const page = document.querySelector(".page");
+const menu = document.querySelector(".menu");
+const navIcon = document.getElementById("nav-icon");
+
